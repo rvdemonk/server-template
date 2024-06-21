959c616e5e1f9035217bd3ec6007d55503dab3a5
rvdemonk <rvdemonk@gmail.com>
2024-06-21 19:18:27 +1000

    nodemon configured

--- scripts/addUsers.js
+++ scripts/addUsers.js
@@ -1,27 +1,26 @@
 const dummy_users = [
-    { username: 'user1', password: 'pass1' },
-    { username: 'user2', password: 'pass2' },
-    { username: 'user3', password: 'pass3' }
+  { username: "user1", password: "pass1" },
+  { username: "user2", password: "pass2" },
+  { username: "user3", password: "pass3" },
 ];
 
-const URL = 'http://localhost:3000/users';
+const URL = "http://localhost:3000/users";
 
 dummy_users.forEach(async (user) => {
-    try {
-        const response = await fetch(URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        });
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
 
-        if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+    if (!response.ok) throw new Error(`Error: ${response.statusText}`);
 
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
-
--- scripts/fetchUsers.js
+++ scripts/fetchUsers.js
@@ -1,10 +1,9 @@
-const URL = 'http://localhost:3000/users';
+const URL = "http://localhost:3000/users";
 
 fetch(URL)
-    .then(response => {
-        if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-
+  .then((response) => {
+    if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
